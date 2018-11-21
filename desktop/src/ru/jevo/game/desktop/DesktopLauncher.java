b981b69f491748bb59734731528514b9d2f4eab0
Jevodan <sanne80@mail.ru>
2018-11-22 00:50:53 +0300

    Initial commit

---
+++ desktop/src/ru/jevo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.jevo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.jevo.game.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
